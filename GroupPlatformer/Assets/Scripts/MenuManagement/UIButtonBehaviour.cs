e7c45fe7afc2f6870a64bd572429475edf9f8efd
Krzwk <44804099+Krzwk@users.noreply.github.com>
2021-02-14 20:51:26 +0100

    UI and Scene Management

---
+++ GroupPlatformer/Assets/Scripts/MenuManagement/UIButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonBehaviour : MonoBehaviour
+{
+    public void LoadLevelByIndex(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    
+}
+
