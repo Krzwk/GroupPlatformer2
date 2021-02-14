ad0bcd933c47b121932f25667c17b7c0e6bc824a
chris <ge64fac@tum.de>
2021-02-14 13:48:05 +0100

    added new Version

---
+++ GroupPlatformer/Assets/Scripts/WallWithButton/ButtonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform initialPlace;
+
+    private float timer = 0.0f;
+    private float waitingTime = 1.5f;
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > waitingTime)
+        {
+            transform.position = initialPlace.position;
+            timer = 0;
+        }
+    }
+}
